eec416fa4780e026ebcd28d5cf6f5dee945cbb25
Toshiaki Nihei <toshiaki.nihei@gmail.com>
2022-01-28 18:09:50 +0900

    process監視スクリプト追加

---
+++ check_process.py
@@ -0,0 +1,9 @@
+import requests
+import settings
+import os
+
+result = os.popen('ps aux | grep python3').read()
+
+quest_count = result.count('quests.py')
+if quest_count < 7:
+  requests.post(settings.DISCORD_URL, { 'content': f'There are some stopped dfk quest processes. quest count: {quest_count}' })
